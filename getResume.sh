e2ca2f5d50ed8f28710dca705e27d4c3c6396aba
Sharwin24 <sharwin24@gmail.com>
2025-08-15 18:43:04 -0500

    Cleaning up

--- getResume.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-src="/home/sharwin/Resume/main.pdf"
-dest="/home/sharwin/sharwin24.github.io/content/resume/SharwinPatilResume.pdf"
-cp "$src" "$dest"
-echo -e "Resume updated from \e[3m/home/sharwin/Resume\e[0m as \e[3mSharwinPatilResume.pdf\e[0m"
